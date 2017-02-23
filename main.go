cef8418c15f32f26365c3edeeaf6438d190f8c44
Robert Laszczak <eventched@gmail.com>
2017-02-23 22:01:36 +0100

    some refactoring

--- main.go
+++ main.go
@@ -1,145 +1,173 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"github.com/Shopify/sarama"
-	"github.com/fatih/color"
-	"gopkg.in/alecthomas/kingpin.v2"
-	"log"
 	"os"
-	"os/signal"
+
+	"github.com/urfave/cli"
+	"github.com/roblaszczak/simple-kafka"
+	"strings"
+	"fmt"
+	"log"
+	"errors"
+	"encoding/json"
+	"bytes"
+	"runtime"
+	"github.com/fatih/color"
+)
+
+const (
+	defaultKafkaPort = "9092"
 )
 
 var (
-	// CLI arguments
-	brokersTCP    = kingpin.Flag("broker", "Broker IP address").Default("127.0.0.1:9092").TCPList()
-	topic         = kingpin.Arg("topic", "Topic to consume.").Required().String()
-	fromBeginning = kingpin.Flag("from-beginning", "If provided all messages from beginning will be consumed").Bool()
-	group         = kingpin.Flag("group", "Consumer group").Default("clikafka").String()
-	verbose       = kingpin.Flag("verbose", "Debug mode").Short('v').Bool()
-	noColor       = kingpin.Flag("no-color", "Disable colored output").Bool()
-	noPrettyJSON = kingpin.Flag("no-pretty-json", "Disable json prettyfication").Bool()
-
-	logger *log.Logger
+	logger       = log.New(os.Stdout, "[clikafka] ", log.LstdFlags|log.Lshortfile)
+	NoBrokersErr = errors.New("you must provide at least one broker using --brokers flag")
 
 	// color functions
 	cyan = color.New(color.FgCyan).SprintFunc()
 )
 
-func setUpLogger() {
-	loggerFlags := log.LstdFlags
-	if *verbose {
-		loggerFlags |= log.Llongfile
-	}
-
-	logger = log.New(os.Stdout, "[clikafka] ", loggerFlags)
-	sarama.Logger = logger
-}
-
-func createConsumer() *sarama.Consumer {
-	brokers := []string{}
-	for _, brokerTCP := range *brokersTCP {
-		brokers = append(brokers, brokerTCP.String())
-	}
-
-	config := sarama.NewConfig()
-	config.ClientID = *group
-
-	consumer, err := sarama.NewConsumer(brokers, config)
-	if err != nil {
-		panic(err)
-	}
-
-	return &consumer
-}
-
-func printMsg(msg *sarama.ConsumerMessage) {
-	var outValue []byte
-
-	if !*noPrettyJSON {
-		var buf bytes.Buffer
-		if err := json.Indent(&buf, msg.Value, "", "\t"); err == nil {
-			outValue = buf.Bytes()
-			outValue = append([]byte{'\n'}, outValue...)
-		}
-	}
-	if outValue == nil {
-		outValue = msg.Value
-	}
-
-	fmt.Printf(
-		"Recieved message - topic: %s, offset: %s, timestamp: %s, value: %s\n",
-		cyan(*topic),
-		cyan(msg.Offset),
-		cyan(msg.Timestamp),
-		cyan(string(outValue)),
-	)
-}
-
-func consumeTopic(consumer sarama.Consumer) {
-	var offset int64
-	if *fromBeginning {
-		offset = sarama.OffsetOldest
-	} else {
-		offset = sarama.OffsetNewest
-	}
-
-	partitionConsumer, err := consumer.ConsumePartition(*topic, 0, offset)
-	if err != nil {
-		panic(err)
-	}
-
-	defer func() {
-		if err := partitionConsumer.Close(); err != nil {
-			logger.Fatalln(err)
-		}
-	}()
-
-	// Trap SIGINT to trigger a shutdown.
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, os.Interrupt)
-
-	consumed := 0
-ConsumerLoop:
-	for {
-		select {
-		case msg := <-partitionConsumer.Messages():
-			printMsg(msg)
-			consumed++
-		case <-signals:
-			break ConsumerLoop
-		}
-	}
-
-	logger.Printf("Consumed: %d\n", consumed)
-}
-
 func main() {
-	kingpin.Version("0.1.0")
-	kingpin.Parse()
-	setUpLogger()
+	app := cli.NewApp()
 
-	if *noColor {
-		color.NoColor = true
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "brokers",
+			Usage: "comma separated brokers list, port is optional, for example --brokers 127.0.0.1:9092,127.0.0.2",
+		},
 	}
 
-	consumer := *createConsumer()
+	app.Commands = []cli.Command{
+		{
+			Name:    "topics",
+			Aliases: []string{"t"},
+			Usage:   "lists topics",
+			Action: func(c *cli.Context) error {
+				client, err := createClient(c)
+				if err != nil {
+					return err
+				}
 
-	defer func() {
-		if err := consumer.Close(); err != nil {
-			logger.Fatalln(err)
-		}
-	}()
+				topics, err := client.Topics()
+				if err != nil {
+					return err
+				}
+				fmt.Println(topics)
 
-	if *verbose {
-		topics, err := consumer.Topics()
-		if err != nil {
-			panic(err)
-		}
-		logger.Printf("Topics: %s", topics)
+				return nil
+			},
+		},
+		{
+			Name:    "consume",
+			Aliases: []string{"c"},
+			Usage:   "consume messages from topic",
+			Flags: []cli.Flag{
+				cli.BoolFlag{
+					Name:  "from-beginning",
+					Usage: "if flag is provided all messages will be shown",
+				},
+				cli.BoolFlag{
+					Name:  "disable-json-pretty",
+					Usage: "this flag disables JSON prettifying",
+				},
+			},
+			Action: func(c *cli.Context) error {
+				client, err := createClient(c)
+				if err != nil {
+					return err
+				}
+
+				var offset int64
+				if c.Bool("from-beginning") {
+					offset = kafka.OffsetEarliest
+				} else {
+					offset = kafka.OffsetLastest
+				}
+
+				topic := c.Args().First()
+				messages, err := client.Consume(topic, offset)
+				if err != nil {
+					return err
+				}
+
+				workersCount := runtime.NumCPU()
+				logger.Printf("creating %d printMessageWorkers", workersCount)
+				workersInput := make([]chan kafka.Message, workersCount)
+
+				for i := 0; i < workersCount; i++ {
+					workersInput[i] = make(chan kafka.Message)
+					go printMessageWorker(i, workersInput[i], c.Bool("disable-json-pretty"))
+				}
+
+				for {
+					for _, c := range workersInput {
+						val := <-messages
+						c <- val
+					}
+				}
+
+				return nil
+			},
+		},
 	}
 
-	consumeTopic(consumer)
+	app.Run(os.Args)
+}
+
+func printMessageWorker(num int, messages <-chan kafka.Message, disableJsonPretty bool) {
+	for message := range messages {
+		rawJson := message.Value
+
+		var parsed string
+		var prettyErr error
+
+		if !disableJsonPretty {
+			var pretty []byte
+			if pretty, prettyErr = prettyJson(rawJson); prettyErr == nil {
+				parsed = string(pretty)
+			}
+		}
+		if prettyErr != nil || disableJsonPretty {
+			parsed = string(rawJson)
+		}
+
+		fmt.Printf(
+			"Recieved message - topic: %s, partition: %s, offset: %s, value: \n%s\n",
+			cyan(message.Topic),
+			cyan(message.Partition),
+			cyan(message.Offset),
+			parsed,
+		)
+	}
+}
+
+func prettyJson(rawJson []byte) ([]byte, error) {
+	var prettyJSONBuf bytes.Buffer
+	err := json.Indent(&prettyJSONBuf, rawJson, "", "\t")
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return prettyJSONBuf.Bytes(), nil
+}
+
+func createClient(c *cli.Context) (kafka.Client, error) {
+	brokersStr := c.GlobalString("brokers")
+	if len(brokersStr) == 0 {
+		return nil, NoBrokersErr
+	}
+	brokers := strings.Split(brokersStr, ",")
+
+	for num, broker := range brokers {
+		brokerParts := strings.Split(broker, ":")
+		if len(brokerParts) == 1 {
+			brokerParts = append(brokerParts, defaultKafkaPort)
+		}
+
+		brokers[num] = strings.Join(brokerParts, ":")
+	}
+
+	logger.Println("creating kafka client for brokers", brokers)
+
+	return kafka.NewClient(brokers)
 }
